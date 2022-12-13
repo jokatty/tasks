35bee8d82d7571abdcfea36ccd1adc61bd8d16be
jokatty <jyotikattani@gmail.com>
2022-12-13 14:51:20 +0530

    render data on the template

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # views
-
 def index(request):
   # return HttpResponse("hi")
-  return render(request,'todo/list.html')
+  # get data from todo table
+  todos = Todo.objects.all()
+  context = {'todos': todos}
+  return render(request,'todo/list.html', context)
