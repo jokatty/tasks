d7b5a81410407fbcfed080605dd7c9e6c550f0d1
jokatty <jyotikattani@gmail.com>
2022-12-13 15:18:32 +0530

    add todo form in views

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # views
 def index(request):
   # return HttpResponse("hi")
   # get data from todo table
   todos = Todo.objects.all()
-  context = {'todos': todos}
+  form = TodoForm()
+  context = {'todos': todos, 'form':form}
   return render(request,'todo/list.html', context)
