3fb0af03069fa52340baeadeabf6c1143aa4c564
jokatty <jyotikattani@gmail.com>
2022-12-13 15:35:01 +0530

    handle post request

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -9,5 +9,12 @@ def index(request):
   # get data from todo table
   todos = Todo.objects.all()
   form = TodoForm()
+  #handle post request
+  if request.method=='POST':
+    form = TodoForm(request.POST)
+    #check if the form has valid data and save it
+    if form.is_valid():
+      form.save()
+    return redirect('/')
   context = {'todos': todos, 'form':form}
   return render(request,'todo/list.html', context)
